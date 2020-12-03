e576ffb9accb75c3e7b11ac054a421faa1f7b621
Cadu <eduardo.w3c@gmail.com>
2020-12-03 17:55:01 -0300

    implements and config mustache

--- api01/src/index.js
+++ api01/src/index.js
@@ -1,18 +1,55 @@
 import express from 'express'
+import path from 'path'
+import mustacheExpress from 'mustache-express'
 import getMessage from './getMessage'
 
-// const express = require('express')
-// const getMessage = require('./getMessage')
 
 const app = express()
 
 const port = 3000
 
-app.get('/', async (req,res)=> {
-    res.send({
-        message: getMessage()
+// CONFIG 
+    // Template engine
+    app.engine('.mustache', mustacheExpress())
+    app.set('view engine', 'mustache')
+    app.set('views', path.join(__dirname, '/views'))
+
+
+app
+    .get('/', async (req,res)=> {
+       
+       let data = [
+           {name:"bulbasaur",experience: 64},
+           {name:"ivysaur",experience: 142},
+           {name:"charmander",experience: 62},
+           {name:"sandshrew",experience: 60},
+           {name:"persian",experience: 158},
+           
+        ]
+
+        res.render('home/index', {
+            data:data
+        })
     })
-})
+
+    .get('/about', async (req,res)=>{
+        let data = [
+            {name:"Jiraya",type:'superhero'},
+            {name:"Changeman",type:'superhero'},
+            {name:"Flashman",type:'superhero'},
+            {name:"Black kamen Rider",type:'superhero'},
+            {name:"Kuwago",type:'moster'},
+            {name:"Ouwashi",type:'moster'},
+            {name:"Oninin Dokusai",type:'enemy'},
+            {name:"Kannin Dragon",type:'enemy'},
+            {name:"Wandar",type:'enemy'},
+            
+         ]
+ 
+         res.render('about/index', {
+             data:data
+         })
+    })
 
 
 app.listen(port,()=>{
