19ef2f5d7f9fd32531ff9ea70532684c113795c2
Cadu <eduardo.w3c@gmail.com>
2020-12-08 19:19:00 -0300

    fix, config form validation

--- api01/db/script.sql
+++ api01/db/script.sql
@@ -4,6 +4,11 @@ create table if not exists news(
     id_news int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title varchar(100),
     news text,
-    createdAt timestamp DEFAULT CURRENT_TIMESTAMP);
+    createdAt timestamp DEFAULT CURRENT_TIMESTAMP,
+    summary varchar(100),
+    author varchar(30),
+    news_date date
+    
+    );
 
 insert into news values(0,'Titulo da noticia','content news','2016-03-13 02:32:21');
--- api01/src/app/router/news.js
+++ api01/src/app/router/news.js
@@ -1,6 +1,7 @@
 import connectionDB from "../../config/connetionDB";
 import { Router } from "express";
 import newsModel from "../models/newsModel";
+import Joi from "joi";
 
 let router = new Router();
 router
@@ -23,17 +24,86 @@ router
     });
   })
   .get("/add", async (req, res) => {
-    // newsModel.getNews(connection, function (error, result) {
-    res.render("news/index");
-    // });
+    res.render("news/add_news");
   })
   .post("/save", async (req, res) => {
-    let news = req.body;
-    let connection = connectionDB();
-
-    newsModel.save(news, connection, function (error, result) {
-      res.redirect("/news");
+    let newsSchema = Joi.object({
+      title: Joi.string().min(2),
+      summary: Joi.string().min(10).max(100).required(),
+      author: Joi.string().required(),
+      news_date: Joi.date().iso().required(),
+      news: Joi.string().required(),
     });
+
+    const options = {
+      abortEarly: false, // include all errors
+      allowUnknown: true, // ignore unknown props
+      stripUnknown: true, // remove unknown props
+    };
+
+    const { error, value } = newsSchema.validate(req.body, options);
+
+    if (error) {
+      res.render("news/add_news", { error: error.details });
+      // next(
+      //   `Validation error: ${error.details.map((x) => x.message).join(", ")}`
+      // );
+    } else {
+      req.body = value;
+      next();
+    }
+    // res.send(error);
   });
+// let news = req.body;
+// let newsSchema = Joi.object().keys({
+//   title: Joi.string().min(2),
+//   summary: Joi.string().min(10).max(100).required(),
+//   author: Joi.string().required(),
+//   news_date: Joi.date().iso().required(),
+//   news: Joi.string().required(),
+// });
+// const result = Joi.validate(news, newsSchema, (err, value) => {
+//   return err;
+// });
+// const { value, error } = result;
+// const valid = error == null;
+// try {
+//   let result = await newsSchema.validateAsync(news);
+//   res.send(result);
+//   res.render("news/add_news", { err: result });
+// } catch (error) {
+//   next(error);
+// }
+// let connection = connectionDB();
+// newsModel.save(news, connection, function (error, result) {
+//   res.redirect("/news");
+// });
+// });
+
+function createNewsSchema(req, res, next) {
+  let newsSchema = Joi.object({
+    title: Joi.string().min(2),
+    summary: Joi.string().min(10).max(100).required(),
+    author: Joi.string().required(),
+    news_date: Joi.date().iso().required(),
+    news: Joi.string().required(),
+  });
+  validateRequest(req, next, res, newsSchema);
+}
+
+function validateRequest(req, next, res, schema) {
+  const options = {
+    abortEarly: false, // include all errors
+    allowUnknown: true, // ignore unknown props
+    stripUnknown: true, // remove unknown props
+  };
+  const { error, value } = schema.validate(req.body, options);
+  if (error) {
+    next(`Validation error: ${error.details.map((x) => x.message).join(", ")}`);
+  } else {
+    req.body = value;
+    next();
+  }
+}
 
 export default router;
